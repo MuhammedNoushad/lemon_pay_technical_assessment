d67d800961d4fdff714be4c41c41120e4abe7777
Muhammed Noushad <mammumuhammed606@gmail.com>
2025-04-29 12:05:57 +0530

    fix: Update axios base URL to use production API endpoint

--- UI/src/axios/axiosInstance.js
+++ UI/src/axios/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://lemon-pay-api.onrender.com",
 });
 
 // Add a request interceptor
